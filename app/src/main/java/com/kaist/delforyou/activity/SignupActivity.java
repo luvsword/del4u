d6b3a5de65dffc2e2a4520c4d0fe71418f5c5403
birmjin10000 <yinbirmjin@gmail.com>
2016-08-05 16:45:42 +0900

    Register User 작업

--- app/src/main/java/com/kaist/delforyou/activity/SignupActivity.java
+++ app/src/main/java/com/kaist/delforyou/activity/SignupActivity.java
@@ -10,6 +10,7 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
@@ -53,9 +54,7 @@ public class SignupActivity extends Activity {
     private EditText inputCPassword;
     private EditText inputPhoneNo;
     private EditText inputEID;
-    private EditText inputCompany;
-    private EditText inputDivision;
-    private EditText inputJobPos;
+    private CheckBox checkboxDeliveryJob;
     private ProgressDialog pDialog;
     private SessionManager session;
     private SQLiteHandler db;
@@ -80,6 +79,9 @@ public class SignupActivity extends Activity {
     HashMap<String, HashMap<String, String>> divisions = new HashMap<>();
     HashMap<String, HashMap<String, String>> buildings = new HashMap<>();
 
+    String divisionID;
+    String buildingID;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -97,6 +99,7 @@ public class SignupActivity extends Activity {
         inputEID = (EditText) findViewById(R.id.employeeid);
         btnRegister = (Button) findViewById(R.id.btnSignupReg);
         btnRegCancel = (Button) findViewById(R.id.btnSignupCancel);
+        checkboxDeliveryJob = (CheckBox) findViewById(R.id.deliveryjob);
 
         // Progress dialog
         pDialog = new ProgressDialog(this);
@@ -125,8 +128,9 @@ public class SignupActivity extends Activity {
                 String cpassword = inputCPassword.getText().toString().trim();
                 String phoneno = inputPhoneNo.getText().toString().trim();
                 String eid = inputEID.getText().toString().trim();
-                String divisionid = "1";
-                String jobposition = "1";
+                String divisionid = divisionID;
+                String buildingid = buildingID;
+                String jobposition = checkboxDeliveryJob.isChecked() ? "000" : "111";
 
                 if (!password.equals(cpassword)) {
                     Toast.makeText(getApplicationContext(),
@@ -135,7 +139,7 @@ public class SignupActivity extends Activity {
                 }
 
                 if (!fname.isEmpty() && !lname.isEmpty() && !email.isEmpty() && !password.isEmpty()) {
-                    registerUser(fname, lname, email, password, phoneno, eid, divisionid, jobposition);
+                    registerUser(fname, lname, email, password, phoneno, eid, divisionid, buildingid, jobposition);
                 } else {
                     Toast.makeText(getApplicationContext(),
                             "Please enter your details!", Toast.LENGTH_LONG)
@@ -148,8 +152,7 @@ public class SignupActivity extends Activity {
         btnRegCancel.setOnClickListener(new View.OnClickListener() {
 
             public void onClick(View view) {
-                Intent i = new Intent(getApplicationContext(),
-                        LoginActivity.class);
+                Intent i = new Intent(getApplicationContext(), LoginActivity.class);
                 startActivity(i);
                 finish();
             }
@@ -162,89 +165,77 @@ public class SignupActivity extends Activity {
      * */
     private void registerUser(final String fname, final String lname, final String email,
                               final String password, final String phoneno, final String eid,
-                              final String divisionid, final String jobposition) {
+                              final String divisionid, final String buildingid, final String jobposition) {
         // Tag used to cancel the request
         String tag_string_req = "req_register";
 
         pDialog.setMessage("Registering ...");
         showDialog();
 
-        StringRequest strReq = new StringRequest(Method.POST,
-                AppConfig.URL_REGISTER, new Response.Listener<String>() {
+        StringRequest strReq = new StringRequest(Method.POST, AppConfig.URL_REGISTER,
+            new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                    Log.d(TAG, "Register Response: " + response.toString());
+                    hideDialog();
 
-            @Override
-            public void onResponse(String response) {
-                Log.d(TAG, "Register Response: " + response.toString());
-                hideDialog();
+                    try {
+                        JSONObject jObj = new JSONObject(response);
+                        boolean error = jObj.getBoolean("error");
+                        if (!error) {
+                            // User successfully stored in MySQL. Now store the user in sqlite
+                            // String uid = jObj.getString("uid");
 
-                try {
-                    JSONObject jObj = new JSONObject(response);
-                    boolean error = jObj.getBoolean("error");
-                    if (!error) {
-                        // User successfully stored in MySQL
-                        // Now store the user in sqlite
-                        //String uid = jObj.getString("uid");
+                            JSONObject user = jObj.getJSONObject("user");
+                            String fname = user.getString("firstname");
+                            String lname = user.getString("lastname");
+                            String email = user.getString("email");
 
-                        JSONObject user = jObj.getJSONObject("user");
-                        String fname = user.getString("firstname");
-                        String lname = user.getString("lastname");
-                        String email = user.getString("email");
+                            // Inserting row in users table
+                            db.addUser(fname, lname, email, eid);
 
-                        // Inserting row in users table
-                        db.addUser(fname, lname, email, eid);
-
-                        Toast.makeText(getApplicationContext(), "User successfully registered. Try login now!", Toast.LENGTH_LONG).show();
-
-                        // Launch login activity
-                        Intent intent = new Intent(
-                                SignupActivity.this,
-                                LoginActivity.class);
-                        startActivity(intent);
-                        finish();
-                    } else {
-
-                        // Error occurred in registration. Get the error
-                        // message
-                        String errorMsg = jObj.getString("error_msg");
-                        Toast.makeText(getApplicationContext(),
-                                errorMsg, Toast.LENGTH_LONG).show();
+                            Toast.makeText(getApplicationContext(), "User successfully registered. Try login now!", Toast.LENGTH_LONG).show();
+                            // Launch login activity
+                            Intent intent = new Intent(SignupActivity.this, LoginActivity.class);
+                            startActivity(intent);
+                            finish();
+                        } else {
+                            // Error occurred in registration. Get the error message
+                            String errorMsg = jObj.getString("error_msg");
+                            Toast.makeText(getApplicationContext(), errorMsg, Toast.LENGTH_LONG).show();
+                        }
+                    } catch (JSONException e) {
+                        e.printStackTrace();
                     }
-                } catch (JSONException e) {
-                    e.printStackTrace();
                 }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Log.e(TAG, "Registration Error: " + error.getMessage());
+                    Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_LONG).show();
+                    hideDialog();
+                }
+            }) {
+                @Override
+                protected Map<String, String> getParams() {
+                    // Posting params to register url
+                    Map<String, String> params = new HashMap<String, String>();
+                    params.put("employeeid", eid);
+                    params.put("firstname", fname);
+                    params.put("lastname", lname);
+                    params.put("phoneno", phoneno);
+                    params.put("divisionid", divisionid);
+                    params.put("buildingid", buildingid);
+                    params.put("jobposition", jobposition);
+                    params.put("email", email);
+                    params.put("password", password);
 
-            }
-        }, new Response.ErrorListener() {
+                    return params;
+                }
+            };
 
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                Log.e(TAG, "Registration Error: " + error.getMessage());
-                Toast.makeText(getApplicationContext(),
-                        error.getMessage(), Toast.LENGTH_LONG).show();
-                hideDialog();
-            }
-        }) {
-
-            @Override
-            protected Map<String, String> getParams() {
-                // Posting params to register url
-                Map<String, String> params = new HashMap<String, String>();
-                params.put("employeeid", eid);
-                params.put("firstname", fname);
-                params.put("lastname", lname);
-                params.put("phoneno", phoneno);
-                params.put("divisionid", divisionid);
-                params.put("jobposition", jobposition);
-                params.put("email", email);
-                params.put("password", password);
-
-                return params;
-            }
-
-        };
-
-        // Adding request to request queue
-        AppController.getInstance().addToRequestQueue(strReq, tag_string_req);
+            // Adding request to request queue
+            AppController.getInstance().addToRequestQueue(strReq, tag_string_req);
     }
 
     private void showDialog() {
@@ -269,7 +260,6 @@ public class SignupActivity extends Activity {
                 public void onItemSelected(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
                     int position= spinner_businessgroup.getSelectedItemPosition();
                     Toast.makeText(getApplicationContext(), "You select " + groupList.toArray()[+position],Toast.LENGTH_SHORT).show();
-
                     fillCompanySpinner(groups.keySet().toArray()[+position].toString());
                 }
                 @Override
@@ -280,7 +270,6 @@ public class SignupActivity extends Activity {
     }
 
     protected void fillCompanySpinner(String groupID) {
-        Log.i("HOHO", "groupID.. " + groupID);
         final HashMap<String, String> companyHashMap = companies.get(groupID);
         final ArrayList<String> companyList = new ArrayList<>(companyHashMap.values());
         ArrayAdapter<String> adapter =
@@ -303,7 +292,6 @@ public class SignupActivity extends Activity {
     }
 
     protected void fillDivisionSpinner(String companyID) {
-        Log.i("HOHO", "companyID) " + companyID);
         final HashMap<String, String> divisionHashMap = divisions.get(companyID);
         final ArrayList<String> divisionList = new ArrayList<>(divisionHashMap.values());
         ArrayAdapter<String> adapter =
@@ -316,7 +304,8 @@ public class SignupActivity extends Activity {
                     public void onItemSelected(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
                         int position= spinner_division.getSelectedItemPosition();
                         Toast.makeText(getApplicationContext(), "You select " + divisionList.toArray()[+position],Toast.LENGTH_SHORT).show();
-                        fillBuildingSpinner(divisionHashMap.keySet().toArray()[+position].toString());
+                        divisionID = divisionHashMap.keySet().toArray()[+position].toString();
+                        fillBuildingSpinner(divisionID);
                     }
                     @Override
                     public void onNothingSelected(AdapterView<?> arg0) {
@@ -326,19 +315,28 @@ public class SignupActivity extends Activity {
     }
 
     protected void fillBuildingSpinner(String divisionID) {
-        Log.i("HOHO", "divisionID) " + divisionID);
-        Log.i("HOHO", "buildingHashMap) " + buildings.toString());
-        HashMap<String, String> buildingHashMap = buildings.get(divisionID);
+        final HashMap<String, String> buildingHashMap = buildings.get(divisionID);
         if (buildingHashMap != null) {
             final ArrayList<String> buildingList = new ArrayList<>(buildingHashMap.values());
             ArrayAdapter<String> adapter =
                     new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, buildingList);
             spinner_building = (Spinner) findViewById(R.id.building);
             spinner_building.setAdapter(adapter);
+            spinner_building.setOnItemSelectedListener(
+                new AdapterView.OnItemSelectedListener() {
+                    @Override
+                    public void onItemSelected(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+                        int position = spinner_building.getSelectedItemPosition();
+                        buildingID = buildingHashMap.keySet().toArray()[+position].toString();
+                    }
+                    @Override
+                    public void onNothingSelected(AdapterView<?> arg0) {
+                    }
+                }
+            );
         } else {
             spinner_building.setAdapter(null);
         }
-
     }
 
     private class PHP_GetCompnayInfo extends AsyncTask<String, Integer, String> {

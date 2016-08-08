cbeb4fba52b81a538e429d953ea21907f69c310c
birmjin10000 <yinbirmjin@gmail.com>
2016-08-08 22:25:43 +0900

    Delivery Person Mani menu complete

--- app/src/main/java/com/kaist/delforyou/app/AppConfig.java
+++ app/src/main/java/com/kaist/delforyou/app/AppConfig.java
@@ -5,15 +5,16 @@ package com.kaist.delforyou.app;
  */
 public class AppConfig {
     // Server user login url
-    public static String URL_LOGIN = "login.php";
+    public static String URL_LOGIN = "";
 
     // Server user register url
     public static String URL_REGISTER = "";
     public static String URL_GETCATINFO = "";
     public static String URL_GETCATINFO1 = "";
     public static String URL_SETREVINFO = "";
-    public static String URL_GETDELIVERY = "delivery_list.php";
-    public static String URL_GETSTATUSINFO = "get_statuslog.php";
+    public static String URL_GETDELIVERY = "";
+    public static String URL_GETSTATUSINFO = "";
     public static String URL_GETDELIVERYINFO;
+    public static String URL_GETDELIVERYJOBS = "";
 }
 

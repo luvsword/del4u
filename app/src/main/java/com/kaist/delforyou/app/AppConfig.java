0e9bfbee791026e185f1fd0c3273191e25599ef6
birmjin10000 <yinbirmjin@gmail.com>
2016-08-09 10:30:06 +0900

    Config URL 추가

--- app/src/main/java/com/kaist/delforyou/app/AppConfig.java
+++ app/src/main/java/com/kaist/delforyou/app/AppConfig.java
@@ -6,18 +6,19 @@ package com.kaist.delforyou.app;
 public class AppConfig {
     public static String HOST ="http://";
     // Server user login url
-    public static String URL_LOGIN = "/login.php";
+    public static String URL_LOGIN = HOST + "/login.php";
     // Server user register url
-    public static String URL_REGISTER = "/register.php";
+    public static String URL_REGISTER = HOST + "/register.php";
     // Server reservation url
-    public static String URL_GETCATINFO = "/get_company.php";
-    public static String URL_GETCATINFO1 = "/company_info.php";
-    public static String URL_SETREVINFO = "/set_reservation.php";
-    public static String URL_GETDELIVERYINFO = "/get_deliveryinfo.php";
-    public static String URL_GETSTATUSINFO = "/get_statuslog.php";
-    public static String URL_GETDELIVERY = "/delivery_list.php";
-    public static String URL_GETDELIVERYJOBS = "/deliveryJob_list.php";
-    public static String URL_GETUNASSIGNED = "/unassigned_deliveries.php";
-    public static String URL_INSERTSTATUSLOG = "/insert_statuslog.php";
+    public static String URL_GETCATINFO = HOST + "/get_company.php";
+    public static String URL_GETCATINFO1 = HOST + "/company_info.php";
+    public static String URL_SETREVINFO = HOST + "/set_reservation.php";
+    public static String URL_GETDELIVERYINFO = HOST + "/get_deliveryinfo.php";
+    public static String URL_GETSTATUSINFO = HOST + "/get_statuslog.php";
+    public static String URL_GETDELIVERY = HOST + "/delivery_list.php";
+    public static String URL_GETDELIVERYJOBS = HOST + "/deliveryJob_list.php";
+    public static String URL_GETUNASSIGNED = HOST + "/unassigned_deliveries.php";
+    public static String URL_INSERTSTATUSLOG = HOST + "/insert_statuslog.php";
+    public static String URL_SETSTATUSINFO = "";
 }
 
